44fbde2634d5e88f84380226789c05412f3c87dc
student <student@gnu.ac.in>
2023-07-18 11:14:57 +0530

    update

---
+++ src/main/kotlin/p1_21012011076.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var a:Int= 22
+    println("Integer Value: $a")
+    var b:Float= 1.5F
+    println("Float Value: $b")
+    var c:Char= 'R'
+    println("Character Value: $c")
+    var d:String= "Hello"
+    println("String Value: $d")
+    var e:Boolean= true
+    println("Boolean Value: $e")
+    var f:Double= 96.36
+    println("Double Value: $f")
+    var g:Long= 123
+    println("Long Value: $g")
+    var h:Short= -2
+    println("Short Value: $h")
+    var i:Byte= 23
+    println("Byte Value: $i")
+
+}
